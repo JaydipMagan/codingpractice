8e899d2092a724dfdce169d3213fbee61a4615e0
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2019-12-27 18:11:33 +0000

    completed map2 problem
    added comments

--- map2.java
+++ map2.java
@@ -6,12 +6,26 @@ wordLen(["this", "and", "that", "and"]) → {"that": 4, "and": 3, "this": 4}
 wordLen(["code", "code", "code", "bug"]) → {"code": 4, "bug": 3}
 */
 
+import java.util.*;
 class map2 {
   public static void main(String[] args) {
-
+    String[] strings = {"a","b","c"};
+    System.out.println(wordLen(strings)); // {"a": 0, "b": 0, "c": 0}
   }
-  public Map<String, Integer> wordLen(String[] strings) {
-
+  /*
+    Function will solve the problem above following the specification.
+    Time complexity is going to be O(n)  n = number of strings in array
+    space complexity is goign to be O(n) where n = number of strings in array,
+    space of map increases linearly.
+  */
+  public static Map<String, Integer> wordLen(String[] strings) {
+    Map<String, Integer> map = new HashMap<String, Integer>();
+    for (String c : strings) {
+      if(!map.containsKey(c)){
+        map.put(c,c.length()); // stores the length of the string
+      }
+    }
+    return map;
   }
 
 }
