0bc5733aef542a8acfa3c81edb09a6baa855c363
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-04-06 13:42:08 +0100

    completed day 6

--- leetcode/30-day/week1/group_anagrams.py
+++ leetcode/30-day/week1/group_anagrams.py
@@ -1,23 +1,13 @@
-import itertools
-from collections import Counter
+import collections
 class Solution:
-    def is_anagram(self,str1, str2):
-        # list_str1 = list(str1)
-        # list_str1.sort()
-        # list_str2 = list(str2)
-        # list_str2.sort()
-
-        # return (list_str1 == list_str2)
-        if len(str1)!=len(str2):
-            return False
-        return Counter(str1) == Counter(str2)
     def groupAnagrams(self, strs):
-        groups = []
+        groups = collections.defaultdict(list)
         for word in strs:
-            group = [x for x in strs if self.is_anagram(word,x)]
-            groups.append(group)
-        groups.sort()
-        return list(k for k,_ in itertools.groupby(groups))
+            key = "".join(sorted(word))
+            groups[key].append(word)
+
+        return groups.values()
+        
 s = Solution()
 print(s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
 print(s.groupAnagrams(["",""]))
