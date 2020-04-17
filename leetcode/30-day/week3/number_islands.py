b38bbd9a20a2bdb1b98f7e521c310f9206732b22
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-04-17 12:06:24 +0100

    finished day 17

---
+++ leetcode/30-day/week3/number_islands.py
@@ -0,0 +1,34 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if len(grid)==0:
+            return 0
+
+        def dfs(i,j,rows,columns,grid):
+            # check if boundary exceeded or point is 0 then backtrack
+            if i<0 or j<0 or i>(rows-1) or j>(columns-1) or grid[i][j]=="0":
+                return 
+            
+            grid[i][j] ="0"
+            
+            # dfs on all directions
+            dfs(i-1,j,rows,columns)
+            dfs(i+1,j,rows,columns)
+            dfs(i,j+1,rows,columns)
+            dfs(i,j-1,rows,columns)
+            
+            
+        
+        islands = 0
+        rows = len(grid)
+        columns = len(grid[0])
+        
+        for i in range(0,len(grid)):
+            row = grid[i]
+            for j in range(0,len(row)):
+                point = row[j]
+                if point=="1":
+                    islands+=1
+                    dfs(i,j,rows,columns)
+                    
+        return islands
+            
