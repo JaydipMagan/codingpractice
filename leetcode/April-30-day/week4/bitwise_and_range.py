2ee013e49a1915c9e2bc9349273484e551309458
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-04-30 13:21:41 +0100

    added problem statements for week4

--- leetcode/April-30-day/week4/bitwise_and_range.py
+++ leetcode/April-30-day/week4/bitwise_and_range.py
@@ -1,3 +1,16 @@
+"""
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+Example 1:
+
+Input: [5,7]
+Output: 4
+
+Example 2:
+
+Input: [0,1]
+Output: 0
+"""
 class Solution:
     def rangeBitwiseAnd(self, m: int, n: int) -> int:
         # return m&n&(self.rangeBitwiseAnd(m>>1,n>>1)<<1) if n>m else n
