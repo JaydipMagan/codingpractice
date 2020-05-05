f77e8c6c601748e85517b59b8da6af56366cf9a4
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-05-05 12:20:10 +0100

    completed day 5

---
+++ leetcode/May-31-day/week1/first_unique_character.py
@@ -0,0 +1,24 @@
+"""
+ Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+
+Note: You may assume the string contain only lowercase letters. 
+"""
+import collections
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        freq = collections.Counter(s)
+        for i in range(0,len(s)):
+            if freq[s[i]]==1:
+                return i
+        return -1
+        
+        
+        
