878323fd6fbfa611c2cea405316fb849e00d2b79
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-05-06 12:54:23 +0100

    finished day 6

---
+++ leetcode/May-31-day/week1/majority_element.py
@@ -0,0 +1,26 @@
+"""
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+
+"""
+import collections
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        freq = collections.Counter(nums)
+        for f in freq:
+            if freq[f]>n//2:
+                return f
+        return -1
