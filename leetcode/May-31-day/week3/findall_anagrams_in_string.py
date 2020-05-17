0ef591e4b693b0c8a7a3304a384047bd8d0e1608
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-05-17 12:12:36 +0100

    added missing import

--- leetcode/May-31-day/week3/findall_anagrams_in_string.py
+++ leetcode/May-31-day/week3/findall_anagrams_in_string.py
@@ -30,6 +30,7 @@ The substring with start index = 0 is "ab", which is an anagram of "ab".
 The substring with start index = 1 is "ba", which is an anagram of "ab".
 The substring with start index = 2 is "ab", which is an anagram of "ab".
 """
+import collections
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         n = len(s)
