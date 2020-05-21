8bd9fbf88c9e0731c6e9dceb8769034d0d581866
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-05-21 13:37:41 +0100

    completed day 21

---
+++ leetcode/May-31-day/week3/count_square_matrix.py
@@ -0,0 +1,68 @@
+"""
+Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
+
+Example 1:
+
+Input: matrix =
+[
+  [0,1,1,1],
+  [1,1,1,1],
+  [0,1,1,1]
+]
+Output: 15
+Explanation: 
+There are 10 squares of side 1.
+There are 4 squares of side 2.
+There is  1 square of side 3.
+Total number of squares = 10 + 4 + 1 = 15.
+
+Example 2:
+
+Input: matrix = 
+[
+  [1,0,1],
+  [1,1,0],
+  [1,1,0]
+]
+Output: 7
+Explanation: 
+There are 6 squares of side 1.  
+There is 1 square of side 2. 
+Total number of squares = 6 + 1 = 7.
+
+Constraints:
+
+    1 <= arr.length <= 300
+    1 <= arr[0].length <= 300
+    0 <= arr[i][j] <= 1
+
+"""
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        rows = len(matrix)
+        if rows==0:
+            return 0
+        cols = len(matrix[0])
+        
+        squares = 0
+        dp = []
+        # print(dp)
+        for r in range(0,rows):
+            dp.append([])
+            for c in range(0,cols):
+                if matrix[r][c]==1:
+                    dp[r].append(1)
+                    squares+=1
+                    if r>0 and c>0:
+                        # print(r,c,matrix[r][c])
+                        min_val = min(dp[r-1][c],dp[r][c-1],dp[r-1][c-1])
+                        dp[r][c]+=min_val
+                        squares+=min_val
+                else:
+                    dp[r].append(0)
+            
+        # print(dp)
+                        
+        return squares  
+                    
+                
