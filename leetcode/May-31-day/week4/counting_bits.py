2368f79f2925b73ebeeb019bfd2eb9878a4fa4d5
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-05-28 12:52:58 +0100

    optimised code

--- leetcode/May-31-day/week4/counting_bits.py
+++ leetcode/May-31-day/week4/counting_bits.py
@@ -30,3 +30,10 @@ class Solution:
             result.append(count)
                 
         return result
+
+
+    def countBitsShorter(self, num: int) -> List[int]:
+        result = [0]*(num+1)
+        for i in range(1,num+1):
+            result[i] = result[i&(i-1)] + 1
+        return result
