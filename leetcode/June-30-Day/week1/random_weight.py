e2c5dc345168f9db9ba89627165f8583ccefcfd5
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-06-05 13:13:29 +0100

    completed day 5

---
+++ leetcode/June-30-Day/week1/random_weight.py
@@ -0,0 +1,53 @@
+"""
+Given an array w of positive integers, where w[i] describes the weight of index i, write a function pickIndex which randomly picks an index in proportion to its weight.
+
+Note:
+
+    1 <= w.length <= 10000
+    1 <= w[i] <= 10^5
+    pickIndex will be called at most 10000 times.
+
+Example 1:
+
+Input: 
+["Solution","pickIndex"]
+[[[1]],[]]
+Output: [null,0]
+
+Example 2:
+
+Input: 
+["Solution","pickIndex","pickIndex","pickIndex","pickIndex","pickIndex"]
+[[[1,3]],[],[],[],[],[]]
+Output: [null,0,1,1,1,0]
+
+Explanation of Input Syntax:
+
+The input is two lists: the subroutines called and their arguments. Solution's constructor has one argument, the array w. pickIndex has no arguments. Arguments are always wrapped with a list, even if there aren't any.
+"""
+import random
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.n = len(w)
+        self.cf = [w[0]] # c.freq
+        for i in range(1,self.n):
+            self.cf.append(self.cf[-1]+w[i])
+        
+    def pickIndex(self) -> int:
+        ran = random.randrange(1, self.cf[-1]+1)
+        l = 0
+        r = len(self.cf) - 1
+        while l < r:
+            mid = l + (r - l) // 2
+            
+            if self.cf[mid] < ran:
+                l = mid + 1
+            else:
+                r = mid
+                
+        return l
+            
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
