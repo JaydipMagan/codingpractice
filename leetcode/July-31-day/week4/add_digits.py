db7111ead64fefb2492b023dea4158daa952102e
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-07-26 14:09:40 +0100

    completed day 26

---
+++ leetcode/July-31-day/week4/add_digits.py
@@ -0,0 +1,15 @@
+"""
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+Example:
+
+Input: 38
+Output: 2 
+Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
+             Since 2 has only one digit, return it.
+"""
+class Solution:
+    def addDigits(self, num: int) -> int:
+        return 0 if num==0 else 1+((num-1)%9)
+        
+            
