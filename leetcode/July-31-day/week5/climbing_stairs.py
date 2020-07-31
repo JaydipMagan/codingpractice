a1412b4531ae0f27a7358a468cb166a2c85ce5d8
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-07-31 19:58:26 +0100

    completed day 31

---
+++ leetcode/July-31-day/week5/climbing_stairs.py
@@ -0,0 +1,37 @@
+"""
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+Constraints:
+
+    1 <= n <= 45
+"""
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n==0:return 0
+        if n<=1:return 1
+        if n<=2:return 2
+        
+        n1, n2 = 1, 1
+        for i in range(0,n):
+            x = n1+n2 
+            n1,n2 = n2, x
+        return n1
